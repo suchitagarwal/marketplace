4b10418e893585f676027a85da8d329a233a76c8
Suchit Agarwal <sagarwal@twitter.com>
2013-11-12 00:16:14 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marketplace::Application.config.secret_token = 'cc0f2111b133a0e1aee5f8e94773ffc126bb23ef3009fbab2bf0de492b066fda87a4cd174311f64c5696a89311f23ac13b2f2cc39619da2f0e8d863692fcfc62'
